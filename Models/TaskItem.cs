c44703b21e99744bbd641eb6f88669022da04471
shx-dow <83426772+shx-dow@users.noreply.github.com>
2025-05-31 00:06:35 +0530

    Update tasks with better priority scheduling

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -3,18 +3,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TaskManager.Models
 {
+    public enum PriorityLevel
+    {
+        Low,
+        Medium,
+        High
+    }
     public class TaskItem
     {
         public int Id { get; set; }
 
         [Required]
-        public string Title { get; set; }
-
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        [Required]
+        public string? Description { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
 
         public bool IsCompleted { get; set; }
+
+        public PriorityLevel Priority { get; set; }
     }
 }
--- obj/Debug/net9.0/ip.AssemblyInfo.cs
+++ obj/Debug/net9.0/ip.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b691b4684782ce9e7f6b9abfbe69c9928a423b94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6862b128aafa7b38d104f984b655d41515b22e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
